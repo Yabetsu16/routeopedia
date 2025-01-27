42d4fa3c9ef437ddb39eb493b7829676d2171583
Yabetsu <it.jabez16@gmail.com>
2025-01-27 21:11:31 +0800

    Link Component to Navigate

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Product = () => {
   const navigate = useNavigate();
   return (
     <div>
       Product
-      <button onClick={() => {navigate("/product/create")}}>Add Product</button>
+      <button
+        onClick={() => {
+          navigate("/product/create");
+        }}
+      >
+        Add Product
+      </button>
+      <Link to={`/product/details/5`}>
+        <button>Navigate to Product Detail - 5</button>
+      </Link>
     </div>
   );
 };
