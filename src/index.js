aa97256f33e147568cab3acdbaeb5223101e1ff1
Yabetsu <it.jabez16@gmail.com>
2025-01-27 15:12:08 +0800

    From Create More Components To Not Found

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Product from "./Pages/Product";
+import CreateProduct from "./Pages/CreateProduct";
+import ProductList from "./Pages/ProductList";
+import ProductDetails from "./Pages/ProductDetails";
+import NotFound from "./NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="product">
+          <Route path="" element={<Product />}></Route>
+          <Route path="create" element={<CreateProduct />}></Route>
+          <Route path="details" element={<ProductDetails />}></Route>
+          <Route path="list" element={<ProductList />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
